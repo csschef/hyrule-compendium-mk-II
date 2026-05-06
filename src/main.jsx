c6856517fd93d7b97a6c19b0958cd7c6f8d5b00a
Sebastian Valdemarsson <valdemarsson.sebastian@gmail.com>
2026-05-06 09:23:04 +0200

    Set basename for BrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/hyrule-compendium-mk-II/">
       <CompendiumProvider>
         <App />
       </CompendiumProvider>
